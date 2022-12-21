9b8702724645d7521768ffab43c8309c0111f61a
Jezer Rázuri <jezerrazuri@upeu.edu.pe>
2022-12-21 17:50:42 -0500

    ASD

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
